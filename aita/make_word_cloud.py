c1f67153cb3fe78dddca4404e0070d05bbc56b81
ywkim <jamie.ywkim@gmail.com>
2019-11-09 15:21:37 -0600

    minor edits

--- aita/make_word_cloud.py
+++ aita/make_word_cloud.py
@@ -17,12 +17,13 @@ subreddit = reddit.subreddit('AmItheAsshole')
 if __name__ == "__main__":
     date = str(datetime.today())[:10]
     text = ''
+
     for submission in subreddit.top(limit=100):
         op_text = submission.selftext.lower()
         text += op_text
 
     stopwords = set(STOPWORDS)
-    for word in ['AITA']:
+    for word in ['AITA', 'said', 'told']:
         stopwords.add(word)
     wc = WordCloud(background_color="white", max_words=100, width=800, height=400,
                    stopwords=stopwords, contour_width=3, contour_color='steelblue')
--- overwatch/overwatch_word_cloud.py
+++ overwatch/overwatch_word_cloud.py
@@ -15,10 +15,10 @@ from wordcloud import WordCloud, STOPWORDS
 reddit = praw.Reddit('search_bot')
 subreddit = reddit.subreddit('OverwatchUniversity')
 
-
 if __name__ == "__main__":
     date = str(datetime.today())[:10]
     text = ''
+
     for submission in subreddit.top(limit=100):
         op_text = submission.selftext.lower()
         text += op_text
@@ -38,5 +38,5 @@ if __name__ == "__main__":
     plt.figure(figsize=(20, 10))
     plt.imshow(wc, interpolation='bilinear')
     plt.axis('off')
-    #plt.show()
+    # plt.show()
     plt.savefig('overwatch_univ_word_cloud_' + date + '.png', dpi=300)
